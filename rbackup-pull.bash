b979707a7781f5e994be644575784f7ee8f2f5e8
Daniel Littlewood <danielittlewood@gmail.com>
2021-12-28 13:58:36 +0000

    Ensure reliable behaviour of  when pulling

--- rbackup-pull.bash
+++ rbackup-pull.bash
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-if [ -n "$1" ]
-then
+if [ -n "$1" ]; then
   mkdir -p $BACKUP_DEST/archives
-  rsync -av $ARCHIVE_DEST/archives/$1 $BACKUP_DEST/archives/$1
+  rsync -av $ARCHIVE_DEST/$1 $BACKUP_DEST/$1
   rmdir $BACKUP_DEST/archives --ignore-fail-on-non-empty # only cleans up if rsync failed
 else
   echo "You must choose an archive to pull."
