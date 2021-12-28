eecf599ce09398bfd786754ac20c1d824c0db122
Daniel Littlewood <danielittlewood@gmail.com>
2021-12-28 18:14:32 +0000

    Add encryption to archival (unconditionally)

--- rbackup-archive.bash
+++ rbackup-archive.bash
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-mkdir -p $BACKUP_DEST/archives
+mkdir -p "$BACKUP_DEST/archives"
 NOW=$(date +%Y%m%d%H%M%S)
-if [ -d $BACKUP_DEST/snapshots ]
-then
-  tar -clf $BACKUP_DEST/archives/$NOW.tar.gz -C $BACKUP_DEST/snapshots . && rm -rf $BACKUP_DEST/snapshots && rm -f $BACKUP_DEST/current
+
+if [ -d "$BACKUP_DEST/snapshots" ]; then
+  tar --remove-files --create --check-links --directory="$BACKUP_DEST/snapshots" . | 
+    gpg --encrypt -r "$GPG_RECIPIENT" --output "$BACKUP_DEST/archives/$NOW.tar.gz.gpg" && 
+    rm -f "$BACKUP_DEST/current"
 else
   echo "ERROR: No snapshots available." 1>&2 # error message, so write to stderr
 fi
