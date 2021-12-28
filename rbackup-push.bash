3378a2f5d7bd3902d955f91ccdcaa6c9652aa8d4
Daniel Littlewood <danielittlewood@gmail.com>
2021-12-28 13:28:09 +0000

    Detect if remote unset when pushing

--- rbackup-push.bash
+++ rbackup-push.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ -d $BACKUP_DEST/archives ]
-then
-  rsync -avzh --remove-source-files --progress $BACKUP_DEST/archives $ARCHIVE_DEST && rmdir $BACKUP_DEST/archives
-else
+if [[ ! -d $BACKUP_DEST/archives ]]; then
   echo "ERROR: No archives available." 1>&2 # error message, so write to stderr
+elif [[ -z "$ARCHIVE_DEST" ]]; then
+  echo "ERROR: No remote specified." 1>&2
+else
+  rsync -avzh --remove-source-files --progress $BACKUP_DEST/archives $ARCHIVE_DEST && rmdir $BACKUP_DEST/archives
 fi
-
