0bbb20d3c29dfb35f1519b64c89639b8937ad904
Daniel Littlewood <danielittlewood@gmail.com>
2022-01-29 22:12:07 +0000

    Prevent archive and backup clashing due to current link still existing

--- rbackup-archive.bash
+++ rbackup-archive.bash
@@ -4,9 +4,9 @@ mkdir -p "$BACKUP_DEST/archives"
 NOW=$(date +%Y%m%d%H%M%S)
 
 if [ -d "$BACKUP_DEST/snapshots" ]; then
+  rm -f "$BACKUP_DEST/current" # if snapshot and archive run simultaneously then current existing will confuse it
   tar --remove-files --create --check-links --gzip --directory="$BACKUP_DEST/snapshots" . | 
-    gpg --encrypt -r "$GPG_RECIPIENT" --output "$BACKUP_DEST/archives/$NOW.tar.gz.gpg" && 
-    rm -f "$BACKUP_DEST/current"
+    gpg --encrypt -r "$GPG_RECIPIENT" --output "$BACKUP_DEST/archives/$NOW.tar.gz.gpg"
 else
   echo "ERROR: No snapshots available." 1>&2 # error message, so write to stderr
 fi
