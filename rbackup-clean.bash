4243ec77b9d811c5e00ea8c21f456355a87fb32f
Daniel Littlewood <danielittlewood@gmail.com>
2022-01-29 22:12:22 +0000

    clean without error messages

--- rbackup-clean.bash
+++ rbackup-clean.bash
@@ -3,6 +3,6 @@
 read -r -p "This will delete everything in $BACKUP_DEST/$1. Are you sure? [y/N] " response
 case "$response" in
     [yY][eE][sS]|[yY]) 
-      rm -r "$BACKUP_DEST/$1"
+      rm -rf "$BACKUP_DEST/$1"
         ;;
 esac
